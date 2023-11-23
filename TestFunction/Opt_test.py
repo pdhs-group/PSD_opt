2babbae8f23ebccdbc33fe2c1968d5227e715ed5
Ji <haoran.ji@kit.edu>
2023-11-23 16:14:46 +0100

    add mean time test

--- TestFunction/Opt_test.py
+++ TestFunction/Opt_test.py
@@ -7,69 +7,117 @@ Created on Tue Nov 21 15:34:14 2023
 import sys
 import os
 import numpy as np
+import time
 sys.path.insert(0,os.path.join(os.path.dirname( __file__ ),".."))
 from kerns_opt import kernel_opt
 
-def noise_test():
-    k=kernel_opt()
-    # k.cal_delta(beta_corr=25, alpha_prim=0.5, first_cal=True)
-    delta = k.cal_delta(beta_corr=25, alpha_prim=0.5)
-    k.visualize_distribution()
-    
-    return delta
-
-def init_points_test(add_noise=True):
-    init_points_opt = 0
-    init_points_max = 10
-    
-    target = np.zeros((3, init_points_max-1))
-    target_diff = np.zeros((3, init_points_max-1))
-    target_opt = 1e10
-    
-    k=kernel_opt(first_cal=True)
-    # Generate artificial data
-    k.add_noise = add_noise
-    k.cal_delta(corr_beta=25, alpha_prim=0.5)
-    
-    k.first_cal = False
-    for i in range(2, init_points_max+1):
-        target[0, i-2], target[1, i-2], target[2, i-2] = k.optimierer(algo='gp_minimize', init_points=i)
-        target_diff[0,i-2] = abs(target[0,i-2] - k.corr_beta) / k.corr_beta
-        target_diff[1,i-2] = abs(target[1,i-2] - k.alpha_prim) / k.alpha_prim
-        target_diff[2,i-2] = target[2,i-2]
+class Opt_test():
+    def __init__(self, generate_data=True):
+        self.dim = 2
+        self.algo='BO'
+        self.delta_flag = 1
+        self.add_noise = True
+        self.corr_beta = 25
+        self.alpha_prim = np.array([0.5, 0.3, 0.1])
         
-        if target_opt > target[2, i-2]:
-            target_opt = target[2, i-2]
-            init_points_opt = i - 2
+        self.k = kernel_opt(dim=self.dim, generate_data=generate_data)
+        self.k.delta_flag = self.delta_flag
+        if generate_data:
+            self.k.add_noise = self.add_noise
+            self.k.cal_delta(corr_beta=self.corr_beta, alpha_prim=self.alpha_prim)
+            self.k.generate_data = False     
+        
+    def noise_test(self):
+        # k.cal_delta(beta_corr=25, alpha_prim=0.5, generate_data=True)
+        delta = self.k.cal_delta(beta_corr=self.corr_beta, alpha_prim=self.alpha_prim)
+        self.k.visualize_distribution()
+        
+        return delta
+    
+    def init_points_test(self, Init_points_max=10):
+        init_points_opt = 0
+        init_points_max = Init_points_max
+        
+        target = np.zeros((3, init_points_max-1))
+        target_diff = np.zeros((3, init_points_max-1))
+        target_opt = 1e10
+
+        for i in range(2, init_points_max+1):
+            start_time = time.time()
             
-    return target, target_opt, init_points_opt, target_diff
+            target[0, i-2], target[1, i-2] = self.k.optimierer(algo=self.algo, init_points=i)
+            target_diff[0,i-2] = \
+                abs(target[0,i-2] - self.k.corr_beta * self.k.alpha_prim) / (self.k.corr_beta * self.k.alpha_prim)
+            target_diff[1,i-2] = target[2,i-2]
+            
+            end_time = time.time()
+            target_diff[2,i-2]  = target[3, i-2] = end_time - start_time
+            
+            if target_opt > target[1, i-2]:
+                target_opt = target[1, i-2]
+                init_points_opt = i - 2
+                
+        return target, target_opt, init_points_opt, target_diff
+    
+    def mean_kernel(self, sample_num=10, same_time=True):
+        para_opt_sample = np.zeros(sample_num)
+        corr_beta_sample = np.zeros(sample_num)
+        if self.dim == 1:
+            alpha_prim_sample = np.zeros(sample_num)
+            
+        if self.dim == 2:
+            alpha_prim_sample = np.zeros((4, sample_num))
+            
+        if same_time:
+            for i in range(sample_num):
+                para_opt_sample[i], _ = self.k.optimierer(algo=self.algo)
+                corr_beta_sample[i] = self.k.corr_beta_opt
+                alpha_prim_sample[:, i] = self.k.alpha_prim_opt
+            
+        para_opt = np.mean(para_opt_sample)
+        corr_beta = np.mean(corr_beta_sample)
+        alpha_prim = np.mean(alpha_prim_sample, axis=1)
+        
+        para_diff = abs(para_opt - self.k.corr_beta * np.sum(self.k.alpha_prim)) / (self.k.corr_beta * np.sum(self.k.alpha_prim))
+        
+        self.k.corr_beta_opt = corr_beta
+        self.k.alpha_prim_opt = alpha_prim
+        self.k.visualize_distribution(new_cal=True)
+        
+        return para_opt, para_diff
+        
+    def opt_test(self):
 
-def opt_test(algo='BO', add_noise=True):
-    k=kernel_opt(first_cal=True)
-    # Generate artificial data
-    k.add_noise = add_noise
-    k.cal_delta(corr_beta=25, alpha_prim=0.5)
-    
-    k.first_cal = False
-    corr_beta_opt, alpha_prim_opt, delta_opt = k.optimierer(algo='gp_minimize')
-    
-    corr_beta_diff = abs(corr_beta_opt - k.corr_beta) / k.corr_beta
-    alpha_prim_diff = abs(alpha_prim_opt - k.alpha_prim) / k.alpha_prim
-    
-    return corr_beta_opt, alpha_prim_opt, delta_opt, corr_beta_diff, alpha_prim_diff    
-
+        para_opt, delta_opt = self.k.optimierer(algo=self.algo)
+        
+        para_diff = abs(para_opt - self.k.corr_beta * np.sum(self.k.alpha_prim)) / (self.k.corr_beta * np.sum(self.k.alpha_prim))
+        
+        self.k.visualize_distribution(new_cal=True)
+        
+        return para_opt, delta_opt, para_diff    
+        
 if __name__ == '__main__':
     
-    test = 2
+    Opt = Opt_test()
+   
+    Opt.dim = 2
+    Opt.algo='BO'
+    Opt.add_noise = True
+    Opt.corr_beta = 25
+    Opt.alpha_prim = np.array([0.5, 0.3, 0.1])
     
+    test = 4
     
     if test == 1:
-        delta = noise_test()
+        delta = Opt.noise_test()
         
     elif test == 2:
-        target, target_opt, init_points_opt, target_diff = init_points_test(add_noise=False)
+        target, target_opt, init_points_opt, target_diff = Opt.init_points_test(Init_points_max=20)
         
     elif test == 3:
-        corr_beta_opt, alpha_prim_opt, delta_opt, corr_beta_diff, alpha_prim_diff = opt_test(algo='gp_minimize', add_noise=False)
+        para_opt, delta_opt, para_diff = Opt.opt_test()
+        
+    elif test == 4:
+        para_opt, para_diff = Opt.mean_kernel(sample_num=10, same_time=True)
 
     
--- kerns_opt.py
+++ kerns_opt.py
@@ -12,20 +12,27 @@ from pop import population
 from bayes_opt import BayesianOptimization
 from skopt import gp_minimize
 from skopt.space import Real
-from functools import partial
+# from functools import partial
+import ast
 from PSD_Exp import write_read_exp
 ## For plots
 import matplotlib.pyplot as plt
 import plotter.plotter as pt          
-from plotter.KIT_cmap import c_KIT_green, c_KIT_red, c_KIT_blue
+# from plotter.KIT_cmap import c_KIT_green, c_KIT_red, c_KIT_blue
 
 class kernel_opt():
-    def __init__(self, dim=1, disc='geo', first_cal=False):
+    def __init__(self, dim=1, disc='geo', generate_data=False):
+        # kernel = 1: optimazition with constant beta and alpha_prim, but too slow, don't use
+        # kernel = 2: optimazition with constant corr_beta and alpha_prim
+        # kernel = 3: optimazition with constant corr_beta and calculated alpha_prim
         self.kernel = 2
-        self.delta_flag = 1
+        # delta_flag = 1: use Q3
+        # delta_flag = 2: use x_50
+        self.delta_flag = 1         
         self.add_noise = True
+        self.noise_type = 'Gaussian'     # Gaussian, Uniform, Poisson, Multiplicative
         self.noise_strength = 0.01
-        self.first_cal = first_cal
+        self.generate_data = generate_data
         self.p = population(dim=dim, disc=disc)
         
         
@@ -33,34 +40,10 @@ class kernel_opt():
         self.filename_kernels = "kernels.txt"
         
     def cal_delta(self, corr_beta=None, alpha_prim=None, scale=1):
-        # Case(1): optimazition with constant beta and alpha_prim, but too slow, don't use
-        # Case(2): optimazition with constant corr_beta and alpha_prim
-        # Case(3): optimazition with constant corr_beta and calculated alpha_prim
         
-        if self.kernel == 1:
-            self.p.COLEVAL = 3
-            self.p.EFFEVAL = 2
-            self.p.CORR_BETA = corr_beta
-            self.p.alpha_prim = alpha_prim
-            self.p.full_init(calc_alpha=False)
-            self.p.solve_PBE()
-            
-        elif self.kernel == 2:
-            self.p.COLEVAL = 2
-            self.p.EFFEVAL = 2
-            self.p.CORR_BETA = corr_beta
-            self.p.alpha_prim = alpha_prim
-            self.p.full_init(calc_alpha=False)
-            self.p.solve_PBE()
-            
-        elif self.kernel == 3:
-            self.p.COLEVAL = 2
-            self.p.EFFEVAL = 2
-            self.p.CORR_BETA = corr_beta
-            self.p.full_init(calc_alpha=True)
-            self.p.solve_PBE()
+        self.cal_pop(corr_beta, alpha_prim)
         
-        if self.first_cal == True:
+        if self.generate_data:
             
             # save the kernels
             with open(self.filename_kernels, 'w') as file:
@@ -76,8 +59,8 @@ class kernel_opt():
             for idt in range(0, len(self.p.t_vec)):
                 _, q3, _, _, _, _ = self.p.return_num_distribution(t=idt)
                 # add noise to the original data
-                if self.add_noise == True:
-                    q3 = self.function_noise(q3)
+                if self.add_noise:
+                    q3 = self.function_noise(q3, noise_type=self.noise_type)
 
                 if len(q3) < len(x_uni):
                     # Pad all arrays to the same length
@@ -105,7 +88,9 @@ class kernel_opt():
                         if 'CORR_BETA:' in line:
                             self.corr_beta = float(line.split(':')[1].strip())
                         elif 'alpha_prim:' in line:
-                            self.alpha_prim = float(line.split(':')[1].strip())
+                            array_str = line.split(':')[1].strip()
+                            array_str = array_str.replace(" ", ", ")
+                            self.alpha_prim = ast.literal_eval(array_str)
             
             x_uni, q3, Q3, x_10, x_50, x_90 = self.p.return_num_distribution(t=len(self.p.t_vec)-1)
             # Conversion unit
@@ -122,8 +107,44 @@ class kernel_opt():
             delta = self.cost_fun(x_uni_exp, x_uni, Q3_exp, Q3, x_50_exp, x_50)
         
             return (delta * scale)
+        
+    def cal_pop(self, corr_beta, alpha_prim):
+        # Case(1): optimazition with constant beta and alpha_prim, but too slow, don't use
+        # Case(2): optimazition with constant corr_beta and alpha_prim
+        # Case(3): optimazition with constant corr_beta and calculated alpha_prim
+        
+        if self.kernel == 1:
+            self.p.COLEVAL = 3
+            self.p.EFFEVAL = 2
+            self.p.CORR_BETA = corr_beta
+            self.p.alpha_prim = alpha_prim
+            self.p.full_init(calc_alpha=False)
+            self.p.solve_PBE()
+            
+        elif self.kernel == 2:
+            self.p.COLEVAL = 2
+            self.p.EFFEVAL = 2
+            self.p.CORR_BETA = corr_beta
+            if self.p.dim == 1:
+                alpha_prim_temp = alpha_prim
+            elif self.p.dim == 2:
+                alpha_prim_temp = np.zeros(4)
+                alpha_prim_temp[0] = alpha_prim[0]
+                alpha_prim_temp[1] = alpha_prim_temp[2] = alpha_prim[1]
+                alpha_prim_temp[3] = alpha_prim[2]
+            self.p.alpha_prim = alpha_prim_temp
+            self.p.full_init(calc_alpha=False)
+            self.p.solve_PBE()
+            
+        elif self.kernel == 3:
+            self.p.COLEVAL = 2
+            self.p.EFFEVAL = 2
+            self.p.CORR_BETA = corr_beta
+            self.p.full_init(calc_alpha=True)
+            self.p.solve_PBE()
     
     def read_exp(self, x_uni, t):
+        
         compare = write_read_exp(self.exp_data_path, read=True)
         q3_exp = compare.get_exp_data(t)
         x_uni_exp = q3_exp.index.to_numpy()
@@ -153,8 +174,18 @@ class kernel_opt():
     
     def optimierer(self, algo='BO', init_points=4, hyperparameter=None):
         if algo == 'BO':
-            pbounds = {'corr_beta': (0, 100), 'alpha_prim': (0, 0.5)}
-            objective = partial(self.cal_delta, scale=-1)
+            if self.p.dim == 1:
+                pbounds = {'corr_beta': (0, 100), 'alpha_prim': (0, 0.5)}
+                objective = lambda corr_beta, alpha_prim: self.cal_delta(
+                    corr_beta=corr_beta, alpha_prim=np.array([alpha_prim]), scale=-1)
+                
+            elif self.p.dim == 2:
+                pbounds = {'corr_beta': (0, 100), 'alpha_prim_0': (0, 0.5), 'alpha_prim_1': (0, 0.5), 'alpha_prim_2': (0, 0.5)}
+                objective = lambda corr_beta, alpha_prim_0, alpha_prim_1, alpha_prim_2: self.cal_delta(
+                    corr_beta=corr_beta, 
+                    alpha_prim=np.array([alpha_prim_0, alpha_prim_1, alpha_prim_1, alpha_prim_2]), 
+                    scale=-1)
+                
             opt = BayesianOptimization(
                 f=objective, 
                 pbounds=pbounds,
@@ -165,25 +196,65 @@ class kernel_opt():
                 init_points=init_points,
                 n_iter=100,
             )   
-            corr_beta_opt = opt.max['params']['corr_beta']
-            alpha_prim_opt = opt.max['params']['alpha_prim']
+            if self.p.dim == 1:
+                para_opt = opt.max['params']['corr_beta'] * opt.max['params']['alpha_prim']
+                self.corr_beta_opt = opt.max['params']['corr_beta']
+                self.alpha_prim_opt = opt.max['params']['alpha_prim']
+                
+            elif self.p.dim == 2:
+                self.alpha_prim_opt = np.zeros(4)
+                para_opt = opt.max['params']['corr_beta'] *\
+                (opt.max['params']['alpha_prim_0'] + 2 * opt.max['params']['alpha_prim_1'] + opt.max['params']['alpha_prim_2'])
+                self.corr_beta_opt = opt.max['params']['corr_beta']
+                self.alpha_prim_opt[0] = opt.max['params']['alpha_prim_0']
+                self.alpha_prim_opt[1] = self.alpha_prim_opt[2] = opt.max['params']['alpha_prim_1']
+                self.alpha_prim_opt[3] = opt.max['params']['alpha_prim_2']
+            
             delta_opt = -opt.max['target']
             
         if algo == 'gp_minimize':
+            if self.p.dim == 1:
+                space = [Real(0, 100), Real(0, 0.5)]
+                objective = lambda params: self.cal_delta(corr_beta=params[0], alpha_prim=np.array([params[1]]), scale=1)
+            elif self.p.dim == 2:
+                space = [Real(0, 100), Real(0, 0.5), Real(0, 0.5), Real(0, 0.5)]
+                objective = lambda params: self.cal_delta(
+                    corr_beta=params[0], 
+                    alpha_prim=np.array([params[1], params[2], params[2], params[3]]), 
+                    scale=1)
+
             opt = gp_minimize(
-                lambda params: self.cal_delta(corr_beta=params[0], alpha_prim=params[1]),
-                [Real(0, 100), Real(0, 0.5)],
+                objective,
+                space,
                 n_calls=100,
                 n_initial_points=init_points,
                 random_state=0
             )
-            corr_beta_opt, alpha_prim_opt = opt.x
+
+            if self.p.dim == 1:
+                para_opt = opt.x[0] * opt.x[1]
+                self.corr_beta_opt = opt.x[0]
+                self.alpha_prim_opt = opt.x[1]
+            elif self.p.dim == 2:
+                self.alpha_prim_opt = np.zeros(4)
+                para_opt = opt.x[0] * (opt.x[1] + 2 * opt.x[2] + opt.x[3])
+                self.corr_beta_opt = opt.x[0]
+                self.alpha_prim_opt[0] = opt.x[1]
+                self.alpha_prim_opt[1] = self.alpha_prim_opt[2] = opt.x[2]
+                self.alpha_prim_opt[3] = opt.x[3]
+
             delta_opt = opt.fun
             
-        return corr_beta_opt, alpha_prim_opt, delta_opt
+        return para_opt, delta_opt
     
-    def visualize_distribution(self, ax=None,fig=None,close_all=False,clr='k',scl_a4=1,figsze=[12.8,6.4*1.5]):
+    def visualize_distribution(self, new_cal=False, ax=None,fig=None,close_all=False,clr='k',scl_a4=1,figsze=[12.8,6.4*1.5]):
         
+        # Recalculate PSD using optimization results
+        if new_cal and hasattr(self, 'corr_beta_opt') and hasattr(self, 'alpha_prim_opt'):
+            self.cal_pop(self.corr_beta_opt, self.alpha_prim_opt)
+        else:
+            print("Need to run the optimization process at least once！")    
+            
         x_uni, q3, Q3, x_10, x_50, x_90 = self.p.return_num_distribution(t=len(self.p.t_vec)-1)
         # Conversion unit
         x_uni *= 1e6    
@@ -208,29 +279,38 @@ class kernel_opt():
         axq3, fig = pt.plot_data(x_uni, q3, fig=fig, ax=axq3,
                                xlbl='Agglomeration size $x_\mathrm{A}$ / $-$',
                                ylbl='volume distribution of agglomerates $q3$ / $-$',
-                               lbl='q3_mod',clr=clr,mrk='o')
+                               lbl='q3_mod',clr='#1f77b4',mrk='o')
         
         axq3, fig = pt.plot_data(x_uni, q3_exp, fig=fig, ax=axq3,
-                               lbl='q3_exp',clr=clr,mrk='^')
+                               lbl='q3_exp',clr='#2ca02c',mrk='^')
         
         axQ3, fig = pt.plot_data(x_uni, Q3, fig=fig, ax=axQ3,
                                xlbl='Agglomeration size $x_\mathrm{A}$ / $-$',
                                ylbl='volume distribution of agglomerates $Q3$ / $-$',
-                               lbl='Q3_mod',clr=clr,mrk='o')
+                               lbl='Q3_mod',clr='#1f77b4',mrk='o')
         axQ3, fig = pt.plot_data(x_uni, Q3_exp, fig=fig, ax=axQ3,
-                               lbl='Q3_exp',clr=clr,mrk='^')
+                               lbl='Q3_exp',clr='#2ca02c',mrk='^')
 
         axq3.grid('minor')
         axQ3.grid('minor')
         plt.tight_layout()   
         
         return axq3, axQ3, fig
+    
+    
     def function_noise(self, ori_data, noise_type='Gaussian'):
         
         if noise_type == 'Gaussian':
             # The first parameter 0 represents the mean value of the noise, 
             # the second parameter is the standard deviation of the noise,
             noise = np.random.normal(0, self.noise_strength, ori_data.shape)
+        elif noise_type == 'Uniform':
+            # Noises are uniformly distributed over the half-open interval [low, high)
+            noise = np.random.uniform(low=-self.noise_strength/2, high=self.noise_strength/2, size=ori_data)
+        elif noise_type == 'Poisson':
+            noise = np.random.poisson(self.noise_strength, ori_data)
+        elif noise_type == 'Multiplicative':
+            noise = np.random.normal(1, self.noise_strength, ori_data.shape) * ori_data - ori_data
 
         return (ori_data + noise)
 
