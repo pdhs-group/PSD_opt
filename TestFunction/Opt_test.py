021683e89ee075ccb6b1f2e1ad79294b36ea51f6
Ji <haoran.ji@kit.edu>
2023-11-27 16:46:14 +0100

    update mean_cal

--- TestFunction/Opt_test.py
+++ TestFunction/Opt_test.py
@@ -8,32 +8,24 @@ import sys
 import os
 import numpy as np
 import time
+import pandas as pd
 sys.path.insert(0,os.path.join(os.path.dirname( __file__ ),".."))
 from kerns_opt import kernel_opt
+from PSD_Exp import write_read_exp
 
 class Opt_test():
-    def __init__(self, add_noise, corr_beta, alpha_prim, noise_type='Gaussian', noise_strength=0.01, t_vec=None, generate_data=True):
+    def __init__(self, add_noise, corr_beta, alpha_prim, delta_flag=1, noise_type='Gaussian', noise_strength=0.01, t_vec=None, generate_data=True):
         self.dim = 2
         self.algo='BO'
-        self.delta_flag = 1
-        self.add_noise = add_noise
-        self.corr_beta = corr_beta
-        self.alpha_prim = alpha_prim
         
-        self.k = kernel_opt(dim=self.dim, t_vec=t_vec, noise_type=noise_type, noise_strength=noise_strength, generate_data=generate_data)
-        self.k.delta_flag = self.delta_flag
+        self.k = kernel_opt(add_noise=add_noise, dim=self.dim, t_vec=t_vec, noise_type=noise_type, noise_strength=noise_strength)
+        self.k.delta_flag = delta_flag
         if generate_data:
-            self.k.add_noise = self.add_noise
-            self.k.cal_delta(corr_beta=self.corr_beta, alpha_prim=self.alpha_prim)
-            self.k.generate_data = False     
-        
-    def noise_test(self):
-        # k.cal_delta(beta_corr=25, alpha_prim=0.5, generate_data=True)
-        delta = self.k.cal_delta(corr_beta=self.corr_beta, alpha_prim=self.alpha_prim)
-        self.k.visualize_distribution()
-        
-        return delta
+            self.k.cal_pop(corr_beta, alpha_prim)
+            self.k.generate_new_data()
     
+    # Test for BO with different number of init_points
+    # only 1D
     def init_points_test(self, Init_points_max=10):
         init_points_opt = 0
         init_points_max = Init_points_max
@@ -58,7 +50,7 @@ class Opt_test():
                 init_points_opt = i - 2
                 
         return target, target_opt, init_points_opt, target_diff
-    
+    # 
     def mean_kernel(self, sample_num=10, method=1):
         if method ==1:
             sample_num=len(self.k.t_vec)
@@ -75,16 +67,80 @@ class Opt_test():
                 para_opt_sample[i], _ = self.k.optimierer(t_step=i, algo=self.algo)
                 corr_beta_sample[i] = self.k.corr_beta_opt
                 alpha_prim_sample[:, i] = self.k.alpha_prim_opt
+            para_opt = np.mean(para_opt_sample)
+            corr_beta = np.mean(corr_beta_sample)
+            alpha_prim = np.mean(alpha_prim_sample, axis=1)
+            self.k.corr_beta_opt = corr_beta
+            self.k.alpha_prim_opt = alpha_prim
+        
+        if method ==2:
+            para_opt_sample = np.zeros(sample_num)
+            corr_beta_sample = np.zeros(sample_num)
+            if self.dim == 1:
+                alpha_prim_sample = np.zeros(sample_num)
+                
+            if self.dim == 2:
+                alpha_prim_sample = np.zeros((4, sample_num))
+                
+            for i in range(0, sample_num):
+                para_opt_sample[i], _ = self.k.optimierer(t_step=len(self.k.t_vec)-1, algo=self.algo)
+                corr_beta_sample[i] = self.k.corr_beta_opt
+                alpha_prim_sample[:, i] = self.k.alpha_prim_opt
+                self.k.generate_new_data()
+
+            para_opt = np.mean(para_opt_sample)
+            corr_beta = np.mean(corr_beta_sample)
+            alpha_prim = np.mean(alpha_prim_sample, axis=1)
+            self.k.corr_beta_opt = corr_beta
+            self.k.alpha_prim_opt = alpha_prim
             
-        para_opt = np.mean(para_opt_sample)
-        corr_beta = np.mean(corr_beta_sample)
-        alpha_prim = np.mean(alpha_prim_sample, axis=1)
+        if method ==3:
+            x_uni, _, _, _, _, _ = self.k.p.return_num_distribution(t=len(self.k.t_vec)-1)
+            x_uni *= 1e6 
+            q3_exp = np.zeros((sample_num, len(x_uni)))
+             
+            for i in range(0, sample_num):
+                if i ==0:
+                    self.k.exp_data_path = self.k.exp_data_path.replace(".xlsx", f"_{i}.xlsx")
+                else:
+                    self.k.exp_data_path = self.k.exp_data_path.replace(f"_{i-1}.xlsx", f"_{i}.xlsx")
+                # print(self.k.exp_data_path)
+                self.k.generate_new_data()
+                '''
+                # Read the data from the last time step and calculate average
+                t = self.k.t_vec[len(self.k.t_vec)-1]   
+                _, q3_exp[i,:], _, _, _, _ = self.k.read_exp(x_uni, t)   
+                
+            q3_exp_mean = np.mean(q3_exp, 0)
+            Q3_exp_mean = q3_exp_mean.cumsum()
+            x_50_exp_mean = np.interp(0.5, Q3_exp_mean, x_uni)
+            para_opt, _ = self.k.optimierer(t_step=len(self.k.t_vec)-1, algo=self.algo, Q3_exp=Q3_exp_mean, x_50_exp=x_50_exp_mean)
+            '''
+            df = pd.DataFrame(index=x_uni)
+            df.index.name = 'Circular Equivalent Diameter'
+            formatted_times = write_read_exp.convert_seconds_to_time(self.k.t_vec)
+
+            for idt in range(0, len(self.k.t_vec)):
+                t = self.k.t_vec[idt]
+                for i in range(0, sample_num):
+                    if i ==0:
+                        self.k.exp_data_path = self.k.exp_data_path.replace(f"_{sample_num-1}.xlsx", f"_{i}.xlsx")
+                    else:
+                        self.k.exp_data_path = self.k.exp_data_path.replace(f"_{i-1}.xlsx", f"_{i}.xlsx")
+                        
+                    _, q3_exp[i,:], _, _, _, _ = self.k.read_exp(x_uni, t)   
+                    
+                q3_exp_mean = np.mean(q3_exp, 0)
+                df[formatted_times[idt]] = q3_exp_mean
+
+            # save DataFrame as Excel file
+            self.k.exp_data_path = self.k.exp_data_path.replace(f"_{sample_num-1}.xlsx", ".xlsx")
+            df.to_excel(self.k.exp_data_path)
+            para_opt, _ = self.k.optimierer(t_step=len(self.k.t_vec)-1, algo=self.algo)
         
         para_diff = abs(para_opt - self.k.corr_beta * np.sum(self.k.alpha_prim)) / (self.k.corr_beta * np.sum(self.k.alpha_prim))
         
-        self.k.corr_beta_opt = corr_beta
-        self.k.alpha_prim_opt = alpha_prim
-        self.k.visualize_distribution(new_cal=True)
+        self.k.visualize_distribution()
         
         return para_opt, para_diff
         
@@ -94,17 +150,21 @@ class Opt_test():
         
         para_diff = abs(para_opt - self.k.corr_beta * np.sum(self.k.alpha_prim)) / (self.k.corr_beta * np.sum(self.k.alpha_prim))
         
-        self.k.visualize_distribution(new_cal=True)
+        self.k.visualize_distribution()
         
         return para_opt, delta_opt, para_diff    
         
 if __name__ == '__main__':
-    add_noise = True
     corr_beta = 25
     alpha_prim = np.array([0.5, 0.3, 0.1])
     t_vec = np.arange(0, 601, 60, dtype=float)
+    
+    # delta_flag = 1: use q3
+    # delta_flag = 2: use Q3
+    # delta_flag = 3: use x_50
+    delta_flag = 1
     # noise_type: Gaussian, Uniform, Poisson, Multiplicative
-    # 
+    add_noise = False
     noise_type='Gaussian'
     noise_strength = 0.01
     
@@ -112,21 +172,19 @@ if __name__ == '__main__':
     Opt.dim = 2
     Opt.algo='BO'
     
-    test = 4
-    
+    test = 1
+        
     if test == 1:
-        delta = Opt.noise_test()
+        para_opt, delta_opt, para_diff = Opt.opt_test()
         
     elif test == 2:
         target, target_opt, init_points_opt, target_diff = Opt.init_points_test(Init_points_max=20)
         
     elif test == 3:
-        para_opt, delta_opt, para_diff = Opt.opt_test()
-        
-    elif test == 4:
         # method=1: Using different time points in a dataset
-        # method=2: Using different datasets at same time points
-        para_opt, para_diff = Opt.mean_kernel(sample_num=10, method=1)
+        # method=2: Using different datasets at same time points, mean kernels
+        # method=3: Using different datasets at same time points, mean datasets
+        para_opt, para_diff = Opt.mean_kernel(sample_num=100, method=3)
     else:
         print('Current test not available')
     
--- kerns_opt.py
+++ kerns_opt.py
@@ -21,18 +21,18 @@ import plotter.plotter as pt
 # from plotter.KIT_cmap import c_KIT_green, c_KIT_red, c_KIT_blue
 
 class kernel_opt():
-    def __init__(self, dim=1, disc='geo', noise_type='Gaussian', noise_strength=0.01, t_vec=None, generate_data=False):
+    def __init__(self, add_noise=True, dim=1, disc='geo', noise_type='Gaussian', noise_strength=0.01, t_vec=None):
         # kernel = 1: optimazition with constant beta and alpha_prim, but too slow, don't use
         # kernel = 2: optimazition with constant corr_beta and alpha_prim
         # kernel = 3: optimazition with constant corr_beta and calculated alpha_prim
         self.kernel = 2
-        # delta_flag = 1: use Q3
-        # delta_flag = 2: use x_50
+        # delta_flag = 1: use q3
+        # delta_flag = 2: use Q3
+        # delta_flag = 3: use x_50
         self.delta_flag = 1         
-        self.add_noise = True
+        self.add_noise = add_noise
         self.noise_type = noise_type    # Gaussian, Uniform, Poisson, Multiplicative
         self.noise_strength = noise_strength
-        self.generate_data = generate_data
         self.t_vec = t_vec
         self.p = population(dim=dim, disc=disc)
         
@@ -52,77 +52,75 @@ class kernel_opt():
         
         self.filename_kernels = "kernels.txt"
         
-    def cal_delta(self, corr_beta=None, alpha_prim=None, scale=1, t_step=0):
+    def cal_delta(self, corr_beta=None, alpha_prim=None, scale=1, t_step=0, Q3_exp=None, x_50_exp=None):
         
         self.cal_pop(corr_beta, alpha_prim)
-        
-        if self.generate_data:
             
-            # save the kernels
-            with open(self.filename_kernels, 'w') as file:
-                file.write('CORR_BETA: {}\n'.format(self.p.CORR_BETA))
-                file.write('alpha_prim: {}\n'.format(self.p.alpha_prim))
+        # read the kernels of original data
+        if not os.path.exists(self.filename_kernels):
+            warnings.warn("file does not exist: {}".format(self.filename_kernels))
             
-            # save the calculation result in experimental data form
-            x_uni, _, _, _, _, _ = self.p.return_num_distribution(t=len(self.p.t_vec)-1)
-            df = pd.DataFrame(index=x_uni*1e6)
-            df.index.name = 'Circular Equivalent Diameter'
-            formatted_times = write_read_exp.convert_seconds_to_time(self.p.t_vec)
-
-            for idt in range(0, len(self.p.t_vec)):
-                _, q3, _, _, _, _ = self.p.return_num_distribution(t=idt)
-                # add noise to the original data
-                if self.add_noise:
-                    q3 = self.function_noise(q3, noise_type=self.noise_type)
-
-                if len(q3) < len(x_uni):
-                    # Pad all arrays to the same length
-                    q3 = np.pad(q3, (0, len(x_uni) - len(q3)), 'constant')
+        else:
+            with open(self.filename_kernels, 'r') as file:
+                lines = file.readlines()  
                 
-                df[formatted_times[idt]] = q3
-
-            # save DataFrame as Excel file
-            df.to_excel(self.exp_data_path)
+                self.corr_beta = None
+                self.alpha_prim = None
+                for line in lines:
+                    if 'CORR_BETA:' in line:
+                        self.corr_beta = float(line.split(':')[1].strip())
+                    elif 'alpha_prim:' in line:
+                        array_str = line.split(':')[1].strip()
+                        array_str = array_str.replace(" ", ", ")
+                        self.alpha_prim = ast.literal_eval(array_str)
+        if t_step >= len(self.p.t_vec):
+            raise ValueError("Current time step is out of the range of the data table.")
             
-            return 
-            
-        else:     
-            # read the kernels of original data
-            if not os.path.exists(self.filename_kernels):
-                warnings.warn("file does not exist: {}".format(self.filename_kernels))
-                
-            else:
-                with open(self.filename_kernels, 'r') as file:
-                    lines = file.readlines()  
-                    
-                    self.corr_beta = None
-                    self.alpha_prim = None
-                    for line in lines:
-                        if 'CORR_BETA:' in line:
-                            self.corr_beta = float(line.split(':')[1].strip())
-                        elif 'alpha_prim:' in line:
-                            array_str = line.split(':')[1].strip()
-                            array_str = array_str.replace(" ", ", ")
-                            self.alpha_prim = ast.literal_eval(array_str)
-            if t_step >= len(self.p.t_vec):
-                raise ValueError("Current time step is out of the range of the data table.")
-                
-            else:
-                x_uni, q3, Q3, x_10, x_50, x_90 = self.p.return_num_distribution(t=t_step)
-                # Conversion unit
-                x_uni *= 1e6    
-                x_10 *= 1e6   
-                x_50 *= 1e6   
-                x_90 *= 1e6   
-                # read and calculate the experimental data
-                t = self.p.t_vec[t_step]
-    
+        else:
+            x_uni, q3, Q3, x_10, x_50, x_90 = self.p.return_num_distribution(t=t_step)
+            # Conversion unit
+            x_uni *= 1e6    
+            x_10 *= 1e6   
+            x_50 *= 1e6   
+            x_90 *= 1e6   
+            # read and calculate the experimental data
+            t = self.p.t_vec[t_step]
+            if Q3_exp == None or x_50_exp == None:
                 x_uni_exp, q3_exp, Q3_exp, x_10_exp, x_50_exp, x_90_exp = self.read_exp(x_uni, t)    
-    
-                # Calculate the error between experimental data and simulation results
-                delta = self.cost_fun(x_uni_exp, x_uni, Q3_exp, Q3, x_50_exp, x_50)
+
+            # Calculate the error between experimental data and simulation results
+            delta = self.cost_fun(q3_exp, q3, Q3_exp, Q3, x_50_exp, x_50)
         
             return (delta * scale)
+        
+    def generate_new_data(self):
+        # save the kernels
+        with open(self.filename_kernels, 'w') as file:
+            file.write('CORR_BETA: {}\n'.format(self.p.CORR_BETA))
+            file.write('alpha_prim: {}\n'.format(self.p.alpha_prim))
+        
+        # save the calculation result in experimental data form
+        x_uni, _, _, _, _, _ = self.p.return_num_distribution(t=len(self.p.t_vec)-1)
+        df = pd.DataFrame(index=x_uni*1e6)
+        df.index.name = 'Circular Equivalent Diameter'
+        formatted_times = write_read_exp.convert_seconds_to_time(self.p.t_vec)
+
+        for idt in range(0, len(self.p.t_vec)):
+            _, q3, _, _, _, _ = self.p.return_num_distribution(t=idt)
+            # add noise to the original data
+            if self.add_noise:
+                q3 = self.function_noise(q3, noise_type=self.noise_type)
+
+            if len(q3) < len(x_uni):
+                # Pad all arrays to the same length
+                q3 = np.pad(q3, (0, len(x_uni) - len(q3)), 'constant')
+            
+            df[formatted_times[idt]] = q3
+
+        # save DataFrame as Excel file
+        df.to_excel(self.exp_data_path)
+        
+        return 
         
     def cal_pop(self, corr_beta, alpha_prim):
         # Case(1): optimazition with constant beta and alpha_prim, but too slow, don't use
@@ -159,6 +157,8 @@ class kernel_opt():
             self.p.full_init(calc_alpha=True)
             self.p.solve_PBE(t_vec=self.t_vec)
     
+    # Read the experimental data and re-interpolate the particle distribution 
+    # of the experimental data according to the simulation results.
     def read_exp(self, x_uni, t):
         
         compare = write_read_exp(self.exp_data_path, read=True)
@@ -176,32 +176,35 @@ class kernel_opt():
         x_50_exp = np.interp(0.5, Q3_exp, x_uni)
         x_90_exp = np.interp(0.9, Q3_exp, x_uni)
         
-        return x_uni_exp, q3_exp, Q3_exp, x_10_exp, x_50_exp, x_90_exp
+        # q3_exp has been redistributed according to x_uni, so actually x_uni rather than x_uni_exp is returned
+        return x_uni, q3_exp, Q3_exp, x_10_exp, x_50_exp, x_90_exp
     
-    def cost_fun(self, x_uni_exp, x_uni, Q3_exp, Q3, x_50_exp, x_50):
-        
-        # delta_flag == 1: use Q3
-        # delta_flag == 2: use x_50
+    def cost_fun(self, q3_exp, q3, Q3_exp, Q3, x_50_exp, x_50):
+        # delta_flag == 1: use q3
+        # delta_flag == 2: use Q3
+        # delta_flag == 3: use x_50
         if self.delta_flag == 1:
+            delta = ((q3*100-q3_exp*100)**2).sum()
+        elif self.delta_flag == 2:
             delta = ((Q3*100-Q3_exp*100)**2).sum()
         else:
             delta = (x_50 - x_50_exp)**2
         
         return delta
     
-    def optimierer(self, algo='BO', t_step=0, init_points=4, hyperparameter=None):
+    def optimierer(self, algo='BO', t_step=0, init_points=4, Q3_exp=None, x_50_exp=None, hyperparameter=None):
         if algo == 'BO':
             if self.p.dim == 1:
                 pbounds = {'corr_beta': (0, 100), 'alpha_prim': (0, 0.5)}
                 objective = lambda corr_beta, alpha_prim: self.cal_delta(
-                    corr_beta=corr_beta, alpha_prim=np.array([alpha_prim]), t_step=t_step, scale=-1)
+                    corr_beta=corr_beta, alpha_prim=np.array([alpha_prim]), t_step=t_step, scale=-1, Q3_exp=None, x_50_exp=None)
                 
             elif self.p.dim == 2:
                 pbounds = {'corr_beta': (0, 100), 'alpha_prim_0': (0, 0.5), 'alpha_prim_1': (0, 0.5), 'alpha_prim_2': (0, 0.5)}
                 objective = lambda corr_beta, alpha_prim_0, alpha_prim_1, alpha_prim_2: self.cal_delta(
                     corr_beta=corr_beta, 
                     alpha_prim=np.array([alpha_prim_0, alpha_prim_1, alpha_prim_1, alpha_prim_2]), 
-                    t_step=t_step, scale=-1)
+                    t_step=t_step, scale=-1, Q3_exp=None, x_50_exp=None)
                 
             opt = BayesianOptimization(
                 f=objective, 
@@ -264,10 +267,20 @@ class kernel_opt():
             
         return para_opt, delta_opt
     
-    def visualize_distribution(self, new_cal=False, ax=None,fig=None,close_all=False,clr='k',scl_a4=1,figsze=[12.8,6.4*1.5]):
-        
+    def visualize_distribution(self, ax=None,fig=None,close_all=False,clr='k',scl_a4=1,figsze=[12.8,6.4*1.5]):
+        # Recalculate PSD using original parameter
+        self.p.CORR_BETA = self.corr_beta
+        self.p.alpha_prim[:] = self.alpha_prim
+        self.p.full_init(calc_alpha=False)
+        self.p.solve_PBE(t_vec=self.t_vec)
+        x_uni_ori, q3_ori, Q3_ori, x_10_ori, x_50_ori, x_90_ori = self.p.return_num_distribution(t=len(self.p.t_vec)-1)
+        # Conversion unit
+        x_uni_ori *= 1e6    
+        x_10_ori *= 1e6   
+        x_50_ori *= 1e6   
+        x_90_ori *= 1e6  
         # Recalculate PSD using optimization results
-        if new_cal and hasattr(self, 'corr_beta_opt') and hasattr(self, 'alpha_prim_opt'):
+        if hasattr(self, 'corr_beta_opt') and hasattr(self, 'alpha_prim_opt'):
             self.cal_pop(self.corr_beta_opt, self.alpha_prim_opt)
         else:
             print("Need to run the optimization process at least once！")    
@@ -296,17 +309,21 @@ class kernel_opt():
         axq3, fig = pt.plot_data(x_uni, q3, fig=fig, ax=axq3,
                                xlbl='Agglomeration size $x_\mathrm{A}$ / $-$',
                                ylbl='volume distribution of agglomerates $q3$ / $-$',
-                               lbl='q3_mod',clr='#1f77b4',mrk='o')
+                               lbl='q3_mod',clr='b',mrk='o')
         
-        axq3, fig = pt.plot_data(x_uni, q3_exp, fig=fig, ax=axq3,
-                               lbl='q3_exp',clr='#2ca02c',mrk='^')
+        axq3, fig = pt.plot_data(x_uni_exp, q3_exp, fig=fig, ax=axq3,
+                               lbl='q3_exp',clr='g',mrk='^')
+        axq3, fig = pt.plot_data(x_uni_ori, q3_ori, fig=fig, ax=axq3,
+                               lbl='q3_ori',clr='r',mrk='v')
         
         axQ3, fig = pt.plot_data(x_uni, Q3, fig=fig, ax=axQ3,
                                xlbl='Agglomeration size $x_\mathrm{A}$ / $-$',
                                ylbl='volume distribution of agglomerates $Q3$ / $-$',
-                               lbl='Q3_mod',clr='#1f77b4',mrk='o')
-        axQ3, fig = pt.plot_data(x_uni, Q3_exp, fig=fig, ax=axQ3,
-                               lbl='Q3_exp',clr='#2ca02c',mrk='^')
+                               lbl='Q3_mod',clr='b',mrk='o')
+        axQ3, fig = pt.plot_data(x_uni_exp, Q3_exp, fig=fig, ax=axQ3,
+                               lbl='Q3_exp',clr='g',mrk='^')
+        axQ3, fig = pt.plot_data(x_uni_ori, Q3_ori, fig=fig, ax=axQ3,
+                               lbl='Q3_ori',clr='r',mrk='v')
 
         axq3.grid('minor')
         axQ3.grid('minor')
