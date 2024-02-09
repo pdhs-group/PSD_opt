5eb73f3e1b95dd0c730c7325d9093e20162fe2f6
HR94 <1015111463@qq.com>
2024-02-09 13:29:29 +0100

    update cell_average for 2d breakage

--- func_test/cell_average_breakage_debug.py
+++ func_test/cell_average_breakage_debug.py
@@ -25,10 +25,10 @@ t = np.arange(0, 1001, 100, dtype=float)
 NS = 30
 S = 2
 R01, R02 = 1, 1
-V01, V02 = 2e-9, 1e-6
-dim = 1
-BREAKFVAL = 2
-BREAKRVAL = 2
+V01, V02 = 2e-9, 2e-9
+dim = 2
+BREAKFVAL = 1
+BREAKRVAL = 1
 
 #%% FUNCTIONS
 def dNdt_1D(t,N,V_p,V_e,B_R,B_F,BREAKFVAL):
@@ -101,6 +101,27 @@ def lam(v, V_p, i, case):
         print('WRONG CASE FOR LAM')
         
 @jit(nopython=True)
+def lam_2d(x,y,Vx,Vy,i,j,m1,m2):
+
+    if m1 == "+":
+        if m2 == "+":
+            lam = (x-Vx[i+1])*(y-Vy[j+1])/((Vx[i]-Vx[i+1])*(Vy[j]-Vy[j+1]))
+        else:
+            lam = (x-Vx[i+1])*(y-Vy[j-1])/((Vx[i]-Vx[i+1])*(Vy[j]-Vy[j-1]))
+    else:
+        if m2 == "+":
+            lam = (x-Vx[i-1])*(y-Vy[j+1])/((Vx[i]-Vx[i-1])*(Vy[j]-Vy[j+1]))
+        else:
+            lam = (x-Vx[i-1])*(y-Vy[j-1])/((Vx[i]-Vx[i-1])*(Vy[j]-Vy[j-1]))
+                
+                
+    if math.isnan(lam):
+        lam = 0 
+        print('lam is NaN!')        
+    
+    return lam  
+        
+@jit(nopython=True)
 def heaviside_jit(x1, x2):
     if x1 < 0:
         return 0.0
@@ -108,7 +129,65 @@ def heaviside_jit(x1, x2):
         return 1.0
     else:
         return x2
+
+def dNdt_2D(t,NN,V_p,V_e1,V_e2,B_R,B_F,BREAKFVAL):
+    N = np.copy(NN) 
+    N = np.reshape(N,(NS,NS))
+    dNdt = np.zeros(N.shape)
+    B_c = np.zeros(N.shape)
+    M1_c = np.zeros(np.shape(N))
+    M2_c = np.zeros(np.shape(N))
+    v1 = np.zeros(np.shape(N))
+    v2 = np.zeros(np.shape(N))
+    D = np.zeros(N.shape)
+    B = np.zeros(N.shape)
     
+    # Loop through all edges
+    # -1 to make sure nothing overshoots (?) CHECK THIS
+    for e1 in range(0, len(V_p)):
+        for e2 in range(0, len(V_p)):
+            if e1 != 0 and e2 !=0:
+                b = B_F[e1,e2,e1,e2]
+                b_int = b_integrate(V_p[e], V_e[e], b)
+                xb_int = xb_integrate(V_p[e], V_e[e], b)
+                B_c[e] += B_R[e]*b_int*N[e]
+                M_c[e] += B_R[e]*xb_int*N[e]
+        
+        ## if breakage function is independent of parent particle, 
+        ## then the its integral only needs to be calculated once
+        # Loop through all pivots (twice)
+        for i in range(e+1, len(V_p)):
+            b = B_F[e,i]
+            b_int = b_integrate(V_e[e+1], V_e[e], b)
+            xb_int = xb_integrate(V_e[e+1], V_e[e], b)
+            B_c[e] += B_R[i]*b_int*N[i]
+            M_c[e] += B_R[i]*xb_int*N[i]
+                    
+        D[e] = -B_R[e]*N[e]
+    D[0] = 0
+    v[B_c != 0] = M_c[B_c != 0]/B_c[B_c != 0]
+    
+    # print(B_c)
+    
+    # Assign BIRTH on each pivot
+    for i in range(len(V_p)):            
+        # Add contribution from LEFT cell (if existent)
+        B[i] += B_c[i]*lam(v[i], V_p, i, 'm')*heaviside_jit(V_p[i]-v[i],0.5)
+        B[i] += B_c[i]*lam(v[i], V_p, i, 'p')*heaviside_jit(v[i]-V_p[i],0.5)
+        if i != 0:
+            # Same Cell, left half
+            # Left Cell, right half
+            B[i] += B_c[i-1]*lam(v[i-1], V_p, i, 'm')*heaviside_jit(v[i-1]-V_p[i-1],0.5)
+            
+        # Add contribution from RIGHT cell (if existent)
+        if i != len(V_p)-1:
+            # Same Cell, right half
+            # Right Cell, left half
+            B[i] += B_c[i+1]*lam(v[i+1], V_p, i, 'p')*heaviside_jit(V_p[i+1]-v[i+1],0.5)
+            
+    dNdt = B + D
+    
+    return dNdt     
 #%% NEW 1D
 if dim == 1:
     # V_e: Volume of EDGES
@@ -131,6 +210,7 @@ if dim == 1:
     
     B_R = np.zeros(NS)
     B_F = np.zeros((NS,NS))
+    
     if BREAKRVAL == 1:
         B_R[1:] = 1
     elif BREAKRVAL == 2:
@@ -202,6 +282,119 @@ if dim == 1:
     
     # ax2.plot(t, mu0_as, color='k', linestyle='-.', label='$\mu_0$ (analytical)')
     # ax2.plot(t, mu1_as, color='b', linestyle='-.', label='$\mu_1$ (analytical)')
+    ax2.plot(t, mu1, color=c_KIT_red, label='$\mu_1$')     
+    # ax2.set_xscale('log')
+    # ax2.set_yscale('log')
+    ax2.legend()
+    plt.tight_layout()
+    
+#%% NEW 2D    
+if dim == 2:
+    # V_e: Volume of EDGES
+    V_e1 = np.zeros(NS+1) #np.zeros(NS+1)
+    V_e2 = np.zeros(NS+1) #np.zeros(NS+1)    
+    
+    # V_p: Volume of PIVOTS
+    V_p1 = np.zeros(NS)#np.zeros(NS)
+    V_p2 = np.zeros(NS)#np.zeros(NS)
+    V_p = np.ones((NS,NS))#np.zeros((NS,NS)) 
+    
+    # Volume fractions
+    X1 = np.zeros((NS,NS))#np.zeros((NS,NS)) 
+    X2 = np.zeros((NS,NS))#np.zeros((NS,NS)) 
+    
+    # SOLUTION N is saved on pivots
+    N = np.zeros((NS,NS,len(t)))#np.zeros((NS,NS,len(t)))
+    N[-1,-1,0] = 1
+    
+    for i in range(1,NS+1):
+        V_e1[i+1] = S**(i-1)*V01
+        V_e2[i+1] = S**(i-1)*V02
+        V_p1[i] = (V_e1[i] + V_e1[i-1]) / 2
+        V_p2[i] = (V_e2[i] + V_e2[i-1]) / 2
+        
+    V_p[:,0] = V_p1 #V_p[:,0] = V_p1  
+    V_p[0,:] = V_p2 #V_p[0,:] = V_p2
+    
+    # Calculate remaining entries of V_e and V_p and other matrices
+    for i in range(NS): #range(NS)
+        for j in range(NS): #range(NS)
+            V_p[i,j] = V_p1[i]+V_p2[j]
+            if i==0 and j==0: #i==0 and j==0
+                X1[i,j] = 0
+                X2[i,j] = 0
+            else:
+                X1[i,j] = V_p1[i]/V_p[i,j]
+                X2[i,j] = V_p2[j]/V_p[i,j]
+    
+    B_R = np.zeros((NS,NS))
+    B_F = np.zeros((NS,NS,NS,NS))
+    
+    if BREAKRVAL == 1:
+        B_R[1:,1:] = 1
+    elif BREAKRVAL == 2:
+        for idx, tmp in np.ndenumerate(B_R):
+            a = idx[0]; b = idx[1]
+            B_R[idx] = V_p1[a]*V_p2[b]
+            
+    for idx, tmp in np.ndenumerate(B_F):
+        a = idx[0]; b = idx[1] ; i = idx[2]; j = idx[3] 
+        if BREAKFVAL == 1:  
+            B_F[idx] = 4 / (V_p1[idx[i]]*V_p2[idx[j]])
+        elif BREAKFVAL == 2:
+            B_F[idx] = 2 / (V_p1[idx[i]]*V_p2[idx[j]])
+            
+    
+    # SOLVE    
+    import scipy.integrate as integrate
+    RES = integrate.solve_ivp(dNdt_2D,
+                              [0, max(t)], 
+                              N[:,:,0].reshape(-1), t_eval=t,
+                              args=(V_p,V_e1,V_e2,B_R,B_F,BREAKFVAL),
+                              method='RK23',first_step=0.1,rtol=1e-3)
+    
+    # Reshape and save result to N and t_vec
+    N = RES.y.reshape((NS,NS,len(t))) #RES.y.reshape((NS,NS,len(t)))
+    t = RES.t
+    
+    N0 = N[:,:,0]
+    NE = N[:,:,-1]
+    print('### Total Volume before and after..')
+    print(np.sum(N0*V_p), np.sum(NE*V_p))
+    
+    print('### Initial dNdt..')
+    dNdt0=dNdt_2D(0,N[:,:,0].reshape(-1),V_p,V_e1,V_e2,B_R,B_F,BREAKFVAL).reshape(NS,NS)#.reshape(NS,NS)
+    print(dNdt0)
+    
+    VE2, VE1 = np.meshgrid(V_e2, V_e1)
+    VP2, VP1 = np.meshgrid(V_p2, V_p1)
+
+    fig=plt.figure(figsize=[5,5])    
+    ax=fig.add_subplot(1,1,1) 
+    ax.scatter(VE2.flatten(), VE1.flatten(), marker='x', color='k', label='edges')    
+    ax.scatter(VP2.flatten(), VP1.flatten(), marker='d', color=c_KIT_green, label='pivots')
+    # ax.set_xscale('log')
+    # ax.set_yscale('log')
+    ax.legend()
+    plt.tight_layout()
+         
+    fig2=plt.figure(figsize=[4,3])    
+    ax2=fig2.add_subplot(1,1,1) 
+    
+    mu1 = np.zeros(t.shape)
+    mu0 = np.zeros(t.shape)
+    for ti in range(len(t)): 
+        mu1[ti] = np.sum(V_p*N[:,:,ti])/np.sum(V_p*N[:,:,0])  
+        mu0[ti] = np.sum(N[:,:,ti])/np.sum(N[:,:,0]) 
+        
+    ax2.plot(t, mu0, color=c_KIT_green, label='$\mu_0$ (numerical)') 
+    mu_as = np.zeros((2,2,len(t)))
+    if BREAKRVAL == 1 and BREAKFVAL == 1:
+        for k in range(2):
+            for l in range(2):
+                mu_as[k,l,:] = np.exp((4/((k+1)*(l+1))-1)*t)
+
+    ax2.plot(t, mu_as[0,0,:], color='k', linestyle='-.', label='$\mu_0$ (analytical)')
     ax2.plot(t, mu1, color=c_KIT_red, label='$\mu_1$')     
     # ax2.set_xscale('log')
     # ax2.set_yscale('log')
